74be10309d4aa53dee1603827e2597a197a121cc
Alex Ryapolov <ryapolov@pm.me>
2026-02-14 16:32:27 +0300

    Add codec/gob implementation

---
+++ codec/gob/codec.go
@@ -0,0 +1,50 @@
+package gob
+
+import (
+	"bytes"
+	"encoding/gob"
+	"io"
+	"iter"
+)
+
+type Codec[Item any] struct {
+	buf *bytes.Buffer
+}
+
+func New[Item any]() *Codec[Item] {
+	return &Codec[Item]{
+		buf: new(bytes.Buffer),
+	}
+}
+
+func (c *Codec[Item]) Encode(batch iter.Seq[Item]) ([]byte, error) {
+	c.buf.Reset()
+	enc := gob.NewEncoder(c.buf)
+
+	for item := range batch {
+		if err := enc.Encode(&item); err != nil {
+			return nil, err
+		}
+	}
+
+	res := c.buf.Bytes()
+	out := make([]byte, len(res))
+	copy(out, res)
+
+	return out, nil
+}
+
+func (c *Codec[Item]) Decode(data []byte, push func(Item)) error {
+	dec := gob.NewDecoder(bytes.NewReader(data))
+
+	for {
+		var item Item
+		err := dec.Decode(&item)
+		if err == io.EOF {
+			return nil
+		} else if err != nil {
+			return err
+		}
+		push(item)
+	}
+}
---
+++ codec/gob/codec_test.go
@@ -0,0 +1,49 @@
+package gob_test
+
+import (
+	"math/rand/v2"
+	"slices"
+	"strconv"
+	"testing"
+
+	"github.com/teenjuna/liq"
+	"github.com/teenjuna/liq/buffer"
+	"github.com/teenjuna/liq/codec/gob"
+	"github.com/teenjuna/liq/internal/testing/require"
+)
+
+var _ liq.Codec[any] = (*gob.Codec[any])(nil)
+
+func TestCodec(t *testing.T) {
+	type Item struct {
+		ID string
+		N1 int
+		N2 float64
+	}
+
+	buffer := buffer.Appending[Item]()
+	codec := gob.New[Item]()
+
+	var items []Item
+	for i := range 1000 {
+		item := Item{
+			ID: strconv.Itoa(i),
+			N1: rand.IntN(1000),
+			N2: rand.Float64() * 1000,
+		}
+		items = append(items, item)
+		buffer.Push(item)
+	}
+
+	data, err := codec.Encode(buffer.Iter())
+	require.Nil(t, err)
+	require.NotEqual(t, len(data), 0)
+
+	buffer.Reset()
+
+	err = codec.Decode(data, buffer.Push)
+	require.Nil(t, err)
+
+	bufferItems := slices.Collect(buffer.Iter())
+	require.Equal(t, bufferItems, items)
+}
